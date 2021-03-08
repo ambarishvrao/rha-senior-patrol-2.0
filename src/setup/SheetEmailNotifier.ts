c28e0c42f18f1fa9e441a5206f17730cc53cbf12
Ambarish Rao <ambarish.r@turvo.com>
2021-03-08 18:27:28 +0530

    Added a try catch to keep sending email for others even if one email send fails

--- src/setup/SheetEmailNotifier.ts
+++ src/setup/SheetEmailNotifier.ts
@@ -20,17 +20,36 @@ function sendEmailsAboutSeniorPatrolSheet() {
         var row = data[i];
         var emailSent = row[3]; // Fourth column
         if (emailSent !== EMAIL_SENT) { // Prevents sending duplicates
-            var emailAddress = row[2]; // Column "C"
-            var message = new String(Templates.citySheetNotification);
-            message = message.replace("{{CITY}}", row[0]); //Column A
-            message = message.replace("{{CITY}}", row[0]); //Column A
-            message = message.replace("{{LINK}}", row[1]);//Column B
-            var subject = '#SeniorPatrol City Requests Sheet';
-            MailApp.sendEmail(emailAddress, subject, message.toString());
-            sheet.getRange(startRow + i, 4).setValue(EMAIL_SENT);
-            // Make sure the cell is updated right away in case the script is interrupted
-            SpreadsheetApp.flush();
-            console.log("Notified " + emailAddress + " successfully");
+            try {
+                var emailAddress = cleanupEmailIds(row[2]); // Column "C"
+                var message = new String(Templates.citySheetNotification);
+                message = message.replace("{{CITY}}", row[0]); //Column A
+                message = message.replace("{{CITY}}", row[0]); //Column A
+                message = message.replace("{{LINK}}", row[1]);//Column B
+                var subject = '#SeniorPatrol City Requests Sheet';
+                MailApp.sendEmail(emailAddress, subject, message.toString());
+                sheet.getRange(startRow + i, 4).setValue(EMAIL_SENT);
+                // Make sure the cell is updated right away in case the script is interrupted
+                SpreadsheetApp.flush();
+                console.log("Notified " + emailAddress + " successfully");
+            } catch (e: unknown) {
+                console.error("error sending email to city= " + row)
+            }
         }
     }
+}
+
+function cleanupEmailIds(raw: string): string {
+    raw = raw.replace(". ", ",");
+    raw = raw.replace("/ ", ",");
+    var emails = raw.split(',');
+    var valid = true;
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    for (var i = 0; i < emails.length; i++) {
+        if (emails[i] === "" || !regex.test(emails[i].replace(/\s/g, ""))) {
+            valid = false;
+        }
+    }
+    return raw;
 }
