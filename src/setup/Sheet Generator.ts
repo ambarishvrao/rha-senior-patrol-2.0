6a70909b6cc2fbd3a3ee692cffa9f44f3f12bb5b
Ambarish Rao <ambarish.r@turvo.com>
2021-03-18 14:50:00 +0530

    Add gwalior as well

--- src/setup/Sheet Generator.ts
+++ src/setup/Sheet Generator.ts
@@ -15,7 +15,7 @@ function createCitySheets() {
   }
   console.log("cityList= " + citylist);
   //City List
-  citylist = ["Nagpur"];
+  citylist = ["Gwalior"];
   //Initializing City URL List
   var urllist = citylist;
   //ID of Sheet where City Sheet URLs will be stored
